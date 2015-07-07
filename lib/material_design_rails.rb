c3559f836dca029fe93d877a76bdc750a15ea11c
Danilo Rodrigues <drodrigues@Danilos-MacBook-Pro.local>
2015-07-07 13:46:14 -0300

    Create the engine file

--- lib/material_design_rails.rb
+++ lib/material_design_rails.rb
@@ -1,4 +1,5 @@
 require "material_design_rails/version"
+require "material_design_rails/engine"
 
 module MaterialDesignRails
   # Your code goes here...
---
+++ lib/material_design_rails/engine.rb
@@ -0,0 +1,4 @@
+module MaterialDesignRails
+  class Engine < Rails::Engine
+  end
+end
